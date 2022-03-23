61abad2980b59e82f80094dadaf8fa809a81fb49
Adam Heeps <adam.heeps@live.co.uk>
2022-03-23 20:51:53 +0000

    added api to get balance

---
+++ account-admin/src/controller/controller.js
@@ -0,0 +1,5 @@
+const getBalance = (req, res) => {
+    res.send("The balance for Adam's account is £100")
+}
+
+module.exports = {getBalance}
--- account-admin/src/router/router.js
+++ account-admin/src/router/router.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("../controller/controller")
 
 router.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+router.get("/api/balance/:id", controller.getBalance)
 
 module.exports = router;
