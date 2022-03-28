d88e8c05ffc889e63ec07c8c3c11a758e9a7b53e
Adam Heeps <adam.heeps@live.co.uk>
2022-03-28 20:44:31 +0100

    added function to get one account by id.

--- account-admin/src/controller/controller.js
+++ account-admin/src/controller/controller.js
@@ -1,5 +1,46 @@
-const getBalance = (req, res) => {
-    res.send("The balance for Adam's account is £100")
+const mongoose = require('mongoose')
+const Account = require('../models/account')
+
+const db =
+  "mongodb+srv://mongo:mongo@customer-database.swf3y.mongodb.net/customerdb?retryWrites=true&w=majority";
+mongoose.connect(db, { useNewUrlParser: true });
+
+const getAllAccounts = (req, res) => {
+    Account.find().sort({name: 'desc'})
+    .then(dbProduct => {
+        res.send(dbProduct)
+    })
+    .catch( err => {
+      res.json(err)
+    })
+  }
+
+const getOneAccount = (req, res) => {
+    Account.findById(req.params.id)
+    .then(dbProduct => {
+        res.send(dbProduct)
+    })
+    .catch(err => {
+        res.json(err)
+    })
 }
 
-module.exports = {getBalance}
+const createNewAccount = (req, res) => {
+    console.log(req.body)
+    Account.create(
+    {
+        name: req.body.name,
+        date_of_birth: req.body.date_of_birth,
+        address: req.body.address,
+        account_balance: req.body.account_balance 
+    }
+  )
+  .then( dbProduct => {
+    res.json(dbProduct)
+  })
+  .catch( err => {
+    res.json(err)
+  });
+}
+
+module.exports = { getAllAccounts, getOneAccount, createNewAccount };
--- account-admin/src/router/router.js
+++ account-admin/src/router/router.js
@@ -2,6 +2,17 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controller/controller")
 
-router.get("/api/balance/:id", controller.getBalance)
+var bodyParser = require('body-parser')
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+router.get("/api/accounts", controller.getAllAccounts)
+router.get("/api/accounts/:id", controller.getOneAccount)
+
+router.post("/api/accounts", urlencodedParser, controller.createNewAccount)
 
 module.exports = router;
