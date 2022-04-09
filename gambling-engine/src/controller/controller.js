a7ea1d641bd742218c2a6f639577f4298049627a
Adam Heeps <adam.heeps@live.co.uk>
2022-04-09 18:09:20 +0100

    can now bet money

--- gambling-engine/src/controller/controller.js
+++ gambling-engine/src/controller/controller.js
@@ -1,36 +1,53 @@
 const serverStatus = (req, res) => {
-  res.status(200).send({message: "Gambling engine running!"})
-}
+  res.status(200).send({ message: "Gambling engine running!" });
+};
 
 const playRoulette = async (req, res) => {
-  wheel = (Math.random() * (36 - 0) + 0).toFixed(0)
-  colour = calculateColour(wheel)
+  wheel = (Math.random() * (36 - 0) + 0).toFixed(0);
+
+  colour = calculateColour(wheel);
 
   if (req.body.colour == undefined) {
-    res.send({message: "Error! Please choose a colour"})
-    return
+    res.send({ message: "Error! Please choose a colour" });
+    return;
   }
-  
+
   if (req.body.colour == colour) {
-    res.send({number: wheel, colour: colour, result: "Congratulations!"})
-    return
+    res.send({
+      number: wheel,
+      colour: colour,
+      winnings: calculateWinnings(req.body.bet, colour),
+      message: "Congratulations! You won!",
+    });
+    return;
   }
   if (req.body.colour != colour) {
-    res.send({number: wheel, colour: colour, result: "Sorry, you lose."})
-    return
-  } 
-}
+    res.send({
+      number: wheel,
+      colour: colour,
+      winnings: 0,
+      message: "Sorry! You lost!",
+    });
+    return;
+  }
+};
 
-const calculateColour = (number) => {
-  if (number == 0) {
-    return "green"
+const calculateColour = (wheelNumber) => {
+  if (wheelNumber == 0) {
+    return "green";
+  } else if (wheelNumber % 2 == 0) {
+    return "red";
+  } else {
+    return "black";
   }
-  else if (number % 2 == 0) {
-    return "red"
-  }
-  else {
-    return "black"
-  }
-}
+};
 
-module.exports = {serverStatus, playRoulette}
+const calculateWinnings = (bet, colour) => {
+  if (colour == "green") {
+    return bet * 14;
+  } else if (colour != "red" || colour != "black") {
+    return bet * 2;
+  }
+};
+
+module.exports = { serverStatus, playRoulette };
