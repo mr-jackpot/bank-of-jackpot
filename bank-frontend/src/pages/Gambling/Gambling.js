b3561528a2bdf44b475f9610d887d862452db129
mr-jackpot <adam.heeps@live.co.uk>
2022-04-30 14:56:56 +0100

    Set up a form on the gambling page

--- bank-frontend/src/pages/Gambling/Gambling.js
+++ bank-frontend/src/pages/Gambling/Gambling.js
@@ -10,6 +10,16 @@ function Gambling() {
     <div>
       <h1>Gambling Home</h1>
       <button onClick={handleClick} type="button">Go back</button>
+
+      <h3>Play Roullete:</h3>
+      <form>
+        <label for="colour">Pick a colour (Red/Black/Green):</label>
+        <input type="radio" id="red" name="colour"></input> 
+        <input type="radio" id="black" name="colour"></input> 
+        <input type="radio" id="green" name="colour"></input> <br/>
+        <label for="bet">Bet amount:</label>
+        <input type="text" id="bet" name="bet"></input>
+      </form>
     </div>
   );
 }
