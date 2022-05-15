9f765b1ecf982dace70ad32b9f4b52f68867409f
mr-jackpot <adam.heeps@live.co.uk>
2022-05-15 14:44:22 +0100

    front end home page styled

--- bank-frontend/src/pages/HomePage/HomePage.js
+++ bank-frontend/src/pages/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router";
+import './HomePage.css'
 
 function HomePage() {
   const navigate = useNavigate();
@@ -10,14 +11,16 @@ function HomePage() {
     navigate("/play");
   };
   return (
-    <div>
-      <h1>Bank home page</h1>
-      <button onClick={handleAccountClick} type="button">
-        Account Overview
-      </button>
-      <button onClick={handleGamblingClick} type="button">
-        Play Gambling
-      </button>
+    <div className="homePage">
+      <p className="welcomeMessage">JACKPOT BANK</p>
+      <div className="buttonContainer">
+        <button className="selectorButton" onClick={handleAccountClick} type="button">
+          Account Overview
+        </button>
+        <button className="selectorButton" onClick={handleGamblingClick} type="button">
+          Play Gambling
+        </button>
+      </div>
     </div>
   );
 }
